7a0599f02b5bcd57d098fe1d68290328cef01578
Jordan-182 <jordan.p62@outlook.fr>
2024-06-09 22:58:24 +0200

    histoire timeline

--- script.js
+++ script.js
@@ -20,3 +20,19 @@ menu.querySelectorAll(".dropdown > i").forEach((arrow) => {
         this.closest(".dropdown").classList.toggle("active");
     });
 });
+
+// ---------- Histoire timeline ---------- //
+
+document.querySelectorAll('.timeline a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
